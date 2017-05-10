6fd204d3286f3c52df1130b1adb49cbf2cf890e4
Reid <reid.wheeler@dal.ca>
2017-05-10 16:29:09 -0300

    Added text field and button which change text on button click

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,6 +2,9 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changeText(View view) {
+        TextView oldText = (TextView)findViewById(R.id.helloText);
+        EditText phrase = (EditText) findViewById(R.id.editText);
+        String newText = phrase.getText().toString();
+        oldText.setText(newText);
     }
 }
